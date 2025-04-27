90a1dbb78438a94e121e637923bda8fe444796eb
Olimdzhon <write2sd@mail.ru>
2025-04-27 16:37:35 +0500

    add cosmwasm;

--- achilles/cmd/achillesd/cmd/commands.go
+++ achilles/cmd/achillesd/cmd/commands.go
@@ -23,6 +23,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
+	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmcli "github.com/CosmWasm/wasmd/x/wasm/client/cli"
 	"github.com/olimdzhon/achilles/app"
 )
 
@@ -49,10 +51,14 @@ func initRootCmd(
 		txCommand(),
 		keys.Commands(),
 	)
+	wasmcli.ExtendUnsafeResetAllCmd(rootCmd)
+
 }
 
 func addModuleInitFlags(startCmd *cobra.Command) {
 	crisis.AddModuleInitFlags(startCmd)
+	wasm.AddModuleInitFlags(startCmd)
+
 }
 
 // genesisCommand builds genesis-related `achillesd genesis` command. Users may provide application specific commands as a parameter
