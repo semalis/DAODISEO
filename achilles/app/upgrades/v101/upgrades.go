0dd664612721288259ac82d6a9f567379f9c8b86
stefan.balea <stefan.balea@axsys.ro>
2025-05-09 09:52:30 +0300

    fixed upgrade handler

---
+++ achilles/app/upgrades/v101/upgrades.go
@@ -0,0 +1,19 @@
+package v100
+
+import (
+	"context"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+)
+
+const UpgradeName = "v1.0.0-rc2"
+
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	cfg module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+		return mm.RunMigrations(ctx, cfg, fromVM)
+	}
+}
